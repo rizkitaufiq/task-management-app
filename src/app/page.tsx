3a29da6f7b37fa18471777a10fac2f4cac338cd5
rizkitaufiqgit config --global user.email <rizkitaufiq97@gmail.com>
2025-06-28 12:52:31 +0700

    chore: initial commit for Next.js task management app

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <section className="p-6">
+      <h1 className="text-2xl font-bold text-center">Task management</h1>
+    </section>
+  );
+}
---
+++ src/components/atoms/button/ButtonSM.tsx
@@ -0,0 +1,11 @@
+// import PropTypes from 'prop-types'
+
+// const ButtonSM = (props) => {
+//   return (
+//     <div>ButtonSM</div>
+//   )
+// }
+
+// ButtonSM.propTypes = {}
+
+// export default ButtonSM
